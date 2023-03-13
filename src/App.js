37d895e3f409e841ec2e02bcf9bb4e2db33c0754
Ramu Singamsetty <s.ramu7799@gmail.com>
2023-03-13 09:47:07 +0530

    Main

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Home from'./Body';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="Home-page">
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Register</li>
+          <li>History</li>
+          <li>Call for papers</li>
+          <li>Program Schedule</li>
+          <li>Contact Us</li>
+        </ul>
+      </nav>
     </div>
   );
 }
+<Home/>
+
 
 export default App;
+
---
+++ src/Body.js
@@ -0,0 +1,16 @@
+
+function Home() {
+    return(
+      <div class="body">
+        <body>
+          <marquee>
+            <img src=""></img>
+            <img src=""></img>
+          </marquee>
+          This is the sample website for the research confrence.
+        </body>
+      </div>
+    )
+  }
+  
+  export default Home();
